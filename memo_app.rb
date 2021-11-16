d8e4d9d2d02bddf9aed9e751057cc1ff00ec764d
Toshiyasu Nonohara <hh0648075@gmail.com>
2021-11-17 06:43:13 +0900

    TOP画面に既存メモのタイトルが一覧表示されるようにした
    各タイトルはそれぞれのメモデータの編集画面へとべるようにするために
    リンクとしている。

--- memo_app.rb
+++ memo_app.rb
@@ -4,7 +4,34 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
+def collect_memo_data
+  file_names = Dir.glob("*", base: "./memos")
+  @memo_files = []
+  file_names.each do |name|
+    @memo_files << File.open("./memos/#{name}") do |file|
+      JSON.load(file)
+    end
+  end
+end
+
 get '/' do
+  collect_memo_data
+  #file_names = Dir.glob("*", base: "./memos")
+  #memo_files = []
+  #file_names.each do |name|
+  #  memo_files << File.open("./memos/#{name}") do |file|
+  #    JSON.load(file)
+  #  end
+  #end
+  @memo_files = @memo_files.sort do |a, b|
+                 a["created_at"] <=> b["created_at"]
+               end
+  @memo_files = @memo_files.reverse
+  @memo_titles = []
+  @memo_files.each do |memo|
+    @memo_titles << memo["title"]
+  end
+  #@memo_titles.sort
   erb :top
 end
 
