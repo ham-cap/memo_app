e1ed348277314cd279fd1e191356dddb95f17df9
Toshiyasu Nonohara <hh0648075@gmail.com>
2021-11-24 06:45:27 +0900

    既存メモの編集機能を実装
    既存メモの内容をフォームの初期値としている。

--- memo_app.rb
+++ memo_app.rb
@@ -3,6 +3,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
+require 'byebug'
 
 def collect_memo_data
   file_names = Dir.glob("*", base: "./memos")
@@ -14,6 +15,12 @@ def collect_memo_data
   end
 end
 
+def sort_by_created_at
+  @memo_files = @memo_files.sort do |a, b|
+                 a["created_at"] <=> b["created_at"]
+               end
+end
+
 get '/' do
   collect_memo_data
   #file_names = Dir.glob("*", base: "./memos")
@@ -23,9 +30,10 @@ get '/' do
   #    JSON.load(file)
   #  end
   #end
-  @memo_files = @memo_files.sort do |a, b|
-                 a["created_at"] <=> b["created_at"]
-               end
+  sort_by_created_at
+  #@memo_files = @memo_files.sort do |a, b|
+  #               a["created_at"] <=> b["created_at"]
+  #             end
   @memo_files = @memo_files.reverse
   @memo_titles = []
   @memo_files.each do |memo|
@@ -40,13 +48,38 @@ get '/new' do
 end
 
 post '/memos' do
-  @id = SecureRandom.uuid
+  id = SecureRandom.uuid
   @title = params[:memo_title]
   @body = params[:memo_body]
   @created_at = Time.now
-  File.open("./memos/memo_#{@id}.json", 'w') do |file|
-    new_memo_data = JSON.dump({"title" => @title, "body" => @body, "created_at" => @created_at}, file)
+  number_of_files = Dir.glob("*", base: "./memos").size
+  @latest_number = number_of_files + 1
+  File.open("./memos/memo_#{@latest_number}.json", 'w') do |file|
+    new_memo_data = JSON.dump({"id" => id, "number" => @latest_number, "title" => @title, "body" => @body, "created_at" => @created_at}, file)
   end
   erb :created 
 end
 
+get '/memos/*' do |num|
+  collect_memo_data
+  sort_by_created_at
+  index = num.to_i - 1
+  selected_memo = @memo_files[index]
+  @selected_number = selected_memo["number"]
+  @selected_title = selected_memo["title"]
+  @selected_body = selected_memo["body"]
+  erb :edit
+end
+
+patch '/memos/:file_number' do
+  @original_file = File.open("./memos/memo_#{params[:file_number]}") do |file|
+    JSON.load(file)
+  end
+  @original_file["title"] = params[:memo_title]
+  @original_file["body"] = params[:memo_body]
+  @original_file["edited_at"] = Time.now
+  File.open("./memos/memo_#{params[:file_number]}", "w") do |file|
+    JSON.dump(@original_file, file)
+  end
+  erb :edited
+end
