2b3b11475160e09b7ae25505f357efd8ac14902b
Toshiyasu Nonohara <hh0648075@gmail.com>
2021-12-06 06:24:35 +0900

    memo_app.rb内の重複している処理をメソッドとして切り出し・一部のviewに「TOPへ戻る」ボタンを追加
    メモの詳細画面を表示する場合と編集画面を表示する場合の処理はその大部分が同じだったため
    メソッドとして切り出して記述量を減らした。
    また、複数のviewにTOP画面へ戻るためのリンクが無かったため追加した。

--- memo_app.rb
+++ memo_app.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 require 'byebug'
+require 'cgi'
 
 def collect_memo_data
   file_names = Dir.glob("*", base: "./memos")
@@ -21,10 +22,23 @@ def sort_by_created_at
                end
 end
 
+def reverse_memos_order
+  @memo_files = @memo_files.reverse
+end
+
+def find_selected_memo(n)
+  index = n.to_i - 1
+  selected_memo = @memo_files[index]
+  @selected_number = selected_memo["number"]
+  @selected_title = selected_memo["title"]
+  @selected_body = selected_memo["body"]
+end
+
 get '/' do
   collect_memo_data
   sort_by_created_at
-  @memo_files = @memo_files.reverse
+  reverse_memos_order
+  #@memo_files = @memo_files.reverse
   @memo_titles = []
   @memo_files.each do |memo|
     @memo_titles << memo["title"]
@@ -38,8 +52,8 @@ end
 
 post '/memos' do
   id = SecureRandom.uuid
-  @title = params[:memo_title]
-  @body = params[:memo_body]
+  @title = CGI.escapeHTML(params[:memo_title])
+  @body = CGI.escapeHTML(params[:memo_body])
   @created_at = Time.now
   number_of_files = Dir.glob("*", base: "./memos").size
   @latest_number = number_of_files + 1
@@ -52,33 +66,35 @@ end
 get '/memos/show/:number' do |n|
   collect_memo_data
   sort_by_created_at
-  index = n.to_i - 1
-  selected_memo = @memo_files[index]
-  @selected_number = selected_memo["number"]
-  @selected_title = selected_memo["title"]
-  @selected_body = selected_memo["body"]
+  #index = n.to_i - 1
+  find_selected_memo(n)
+  #selected_memo = @memo_files[index]
+  #@selected_number = selected_memo["number"]
+  #@selected_title = selected_memo["title"]
+  #@selected_body = selected_memo["body"]
   erb :show
 end
 
 get '/memos/edit/:number' do |n|
   collect_memo_data
   sort_by_created_at
-  index = n.to_i - 1
-  selected_memo = @memo_files[index]
-  @selected_number = selected_memo["number"]
-  @selected_title = selected_memo["title"]
-  @selected_body = selected_memo["body"]
+  #index = n.to_i - 1
+  find_selected_memo(n)
+  #selected_memo = @memo_files[index]
+  #@selected_number = selected_memo["number"]
+  #@selected_title = selected_memo["title"]
+  #@selected_body = selected_memo["body"]
   erb :edit
 end
 
 patch '/memos/:file_number' do
-  @original_file = File.open("./memos/memo_#{params[:file_number]}") do |file|
+  @original_file = File.open("./memos/memo_#{params[:file_number]}.json") do |file|
     JSON.load(file)
   end
-  @original_file["title"] = params[:memo_title]
-  @original_file["body"] = params[:memo_body]
+  @original_file["title"] = CGI.escapeHTML(params[:memo_title])
+  @original_file["body"] = CGI.escapeHTML(params[:memo_body])
   @original_file["edited_at"] = Time.now
-  File.open("./memos/memo_#{params[:file_number]}", "w") do |file|
+  File.open("./memos/memo_#{params[:file_number]}.json", "w") do |file|
     JSON.dump(@original_file, file)
   end
   erb :edited
