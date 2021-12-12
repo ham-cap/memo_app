3d514ce5d36545202ed0b30ab74d4a88dbe3e2a9
Toshiyasu Nonohara <hh0648075@gmail.com>
2021-12-12 09:33:58 +0900

    rubocopによる指摘を反映
    主な変更点は以下の2点
    ・メモデータを読み込む際のメソッドをJSON.loadではなくJSON.parseに変更
    ・取得したメモデータを作成日順に並べ替える際に使用するメソッドをsortからsort_byに変更

--- memo_app.rb
+++ memo_app.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'sinatra'
 require 'sinatra/reloader'
@@ -7,31 +8,30 @@ require 'byebug'
 require 'cgi'
 
 def collect_memo_data
-  file_names = Dir.glob("*", base: "./memos")
+  file_names = Dir.glob('*', base: './memos')
   @memo_files = []
   file_names.each do |name|
     @memo_files << File.open("./memos/#{name}") do |file|
-      JSON.load(file)
+      read_line = file.read
+      JSON.parse(read_line)
     end
   end
 end
 
 def sort_by_created_at
-  @memo_files = @memo_files.sort do |a, b|
-                 a["created_at"] <=> b["created_at"]
-               end
+  @memo_files = @memo_files.sort_by { |a| a['created_at'] }
 end
 
 def reverse_memos_order
   @memo_files = @memo_files.reverse
 end
 
-def find_selected_memo(n)
-  index = n.to_i - 1
+def find_selected_memo(number)
+  index = number.to_i - 1
   selected_memo = @memo_files[index]
-  @selected_number = selected_memo["number"]
-  @selected_title = selected_memo["title"]
-  @selected_body = selected_memo["body"]
+  @selected_number = selected_memo['number']
+  @selected_title = selected_memo['title']
+  @selected_body = selected_memo['body']
 end
 
 get '/' do
@@ -40,7 +40,7 @@ get '/' do
   reverse_memos_order
   @memo_titles = []
   @memo_files.each do |memo|
-    @memo_titles << memo["title"]
+    @memo_titles << memo['title']
   end
   erb :top
 end
@@ -54,36 +54,37 @@ post '/memos' do
   @title = CGI.escapeHTML(params[:memo_title])
   @body = CGI.escapeHTML(params[:memo_body])
   @created_at = Time.now
-  number_of_files = Dir.glob("*", base: "./memos").size
+  number_of_files = Dir.glob('*', base: './memos').size
   @latest_number = number_of_files + 1
   File.open("./memos/memo_#{@latest_number}.json", 'w') do |file|
-    new_memo_data = JSON.dump({"id" => id, "number" => @latest_number, "title" => @title, "body" => @body, "created_at" => @created_at}, file)
+    JSON.dump({ 'id' => id, 'number' => @latest_number, 'title' => @title, 'body' => @body, 'created_at' => @created_at }, file)
   end
-  erb :created 
+  erb :created
 end
 
-get '/memos/show/:number' do |n|
+get '/memos/show/:number' do |number|
   collect_memo_data
   sort_by_created_at
-  find_selected_memo(n)
+  find_selected_memo(number)
   erb :show
 end
 
-get '/memos/edit/:number' do |n|
+get '/memos/edit/:number' do |number|
   collect_memo_data
   sort_by_created_at
-  find_selected_memo(n)
+  find_selected_memo(number)
   erb :edit
 end
 
 patch '/memos/:file_number' do
   @original_file = File.open("./memos/memo_#{params[:file_number]}.json") do |file|
-    JSON.load(file)
+    read_line = file.read
+    JSON.parse(read_line)
   end
-  @original_file["title"] = CGI.escapeHTML(params[:memo_title])
-  @original_file["body"] = CGI.escapeHTML(params[:memo_body])
-  @original_file["edited_at"] = Time.now
-  File.open("./memos/memo_#{params[:file_number]}.json", "w") do |file|
+  @original_file['title'] = CGI.escapeHTML(params[:memo_title])
+  @original_file['body'] = CGI.escapeHTML(params[:memo_body])
+  @original_file['edited_at'] = Time.now
+  File.open("./memos/memo_#{params[:file_number]}.json", 'w') do |file|
     JSON.dump(@original_file, file)
   end
   erb :edited
